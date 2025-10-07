ab78aea29e97c703f055bd6da1a24087020a4b7e
xx1roch <kirillmerkulov44@gmail.com>
2025-10-08 04:49:12 +0500

    Update basic_gui.lua

--- examples/basic_gui.lua
+++ examples/basic_gui.lua
@@ -7,6 +7,8 @@ local Sec = Tab:NewSection("Компоненты", "")
 Sec:NewToggle("АвтоФарм", false, function(state) print("Фарм:", state) end)
 Sec:NewButton("Телепорт", function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0,50,0) end)
 Sec:NewColorPicker("Цвет", Color3.new(1,0,0), function(color) print("Новый цвет:", color) end)
+Sec:NewSlider("Скорость", 16, 100, 16, function(value) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value end)
+Sec:NewDropdown("Оружие", {"Пистолет", "Ружьё", "Граната"}, "Пистолет", function(option) print("Выбрано:", option) end)
 
 YOXI.Notification("Готово!", "GUI загружен", 2)
-YOXI.SetTheme("Light")
+YOXI.LoadTheme("dark") -- Предполагается, что есть themes/dark.json
